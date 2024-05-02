e77f0653d35b920763e0f0018e00d551ee4706a3
anuruddhhit07 <88274721+anuruddhhit07@users.noreply.github.com>
2024-05-02 14:49:54 +0900

    121344

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,8 @@
 function add({ a, b }) {
     return a + b;
 }
+function add2(a, b) {
+    return a + b;
+}
 
-export { add };
+export { add, add2 };
--- src/mathfn.ts
+++ src/mathfn.ts
@@ -2,3 +2,7 @@ import { addtype } from "./types/customtype"
 export  function add({a,b}:addtype):number{
     return a+b
 }
+
+export  function add2(a:number,b:number):number{
+    return a+b
+}
