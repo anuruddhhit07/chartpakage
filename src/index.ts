401b054aaacf20c643da26ff982dc0145532af4e
anuruddhhit07 <88274721+anuruddhhit07@users.noreply.github.com>
2024-05-02 14:29:01 +0900

    first

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// src/main.js
+// import {add} from './foo.js';
+// export default function () {
+// 	console.log(add);
+// }
+export * from "./mathfn"
---
+++ src/types/customtype.ts
@@ -0,0 +1,4 @@
+export type addtype ={
+    a: number;
+    b: number;
+}
