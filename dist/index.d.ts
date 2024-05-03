e7b584fd90c0311f7343c054621f24d6460b519d
anuruddhhit07 <88274721+anuruddhhit07@users.noreply.github.com>
2024-05-03 14:29:40 +0900

    234324

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,2 @@
-import TESTOHLCVDATA from "./testdata/testdataapi";
 export { add, add2 } from "./mathcom/mathfn";
 export { drawLineChart } from "./chartsvg/createchart";
-export { TESTOHLCVDATA };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import TESTOHLCVDATA from "./testdata/testdataapi";
+
 
 
 export { add, add2 } from "./mathcom/mathfn"
 export { drawLineChart } from "./chartsvg/createchart";
-export {TESTOHLCVDATA} 
